d9863df776d2ce6298d8ca3e17427cb6c787684d
Campbell Morgan <campbell@folissimo.com>
2013-01-16 13:30:00 +0000

    first entry

---
+++ content/firstEntry.md
@@ -0,0 +1,9 @@
+Title: First Entry in the Folissimo Blog
+Date: 2013-01-16 09:30
+Tags: yes, whatever
+Category: introduction
+Slug: first-entry
+Author: Campbell Morgan
+Summary: Article Summary
+
+Testing testing testing
---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Campbell Morgan'
+SITENAME = 'Folissimo Blog'
+SITEURL = 'http://blog.folissimo.com'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Folissimo', 'http://www.folissimo.com'),)
+
+THEME = './theme'
+
+# Social widget
+SOCIAL = (('Twitter', 'http://www.twitter.com/folissimo'),
+          ('Facebook', 'http://www.facebook.com/folissimo'),
+          ('Pinterest', 'http://www.pinterest.com/folissimo'),)
+
+DEFAULT_PAGINATION = False
